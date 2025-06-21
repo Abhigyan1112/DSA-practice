3658872de5a4aa979512f9e32a6b08f624e90323
Abhigyan1112 <abhigyansharma.hhh@gmail.com>
2025-06-21 13:27:17 +0530

    hello

---
+++ Graphs/BellmanFord.java
@@ -0,0 +1,32 @@
+// helps to detect negative cycles
+import java.util.*;
+public class BellmanFord extends Solution{
+    public static void main(String[] args) {
+        
+    }
+}
+// User function Template for Java
+
+class Solution {
+    public int[] bellmanFord(int V, int[][] edges, int src) {
+        // code here
+        int[] min=new int[V];
+        int mod=(int)Math.pow(10,8);
+        Arrays.fill(min,mod);
+        min[src]=0;
+        for(int i=1;i<V;i++){
+            for(int[] edge:edges){
+                if(min[edge[0]]!=mod && min[edge[0]]+edge[2]<min[edge[1]]){
+                    min[edge[1]]=min[edge[0]]+edge[2];
+                }
+            }
+        }
+        for(int[] edge:edges){
+            if(min[edge[0]]!=mod && min[edge[0]]+edge[2]<min[edge[1]]){
+                int[] temp={-1};
+                return temp;
+            }
+        }
+        return min;
+    }
+}
